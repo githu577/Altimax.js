6b2ace17809326856ffe3ea5fac1f06d11ca0358
githu577 <github1234512345@gmail.com>
2026-07-28 16:36:30 +0600

    Add files via upload

---
+++ scripts/cmds/owner (1).js
@@ -0,0 +1,64 @@
+const fs = require("fs-extra");
+const request = require("request");
+const path = require("path");
+
+module.exports = {
+  config: {
+    name: "owner",
+    version: "1.3.0",
+    author: "Mᴏʜᴀᴍᴍᴀᴅ Aᴋᴀsʜ",
+    role: 0,
+    shortDescription: "Owner information with image",
+    category: "Information",
+    guide: {
+      en: "owner"
+    }
+  },
+
+  onStart: async function ({ api, event }) {
+    const ownerText = 
+`┌───────────────⭓
+│ 𝗢𝗪𝗡𝗘𝗥 𝗗𝗘𝗧𝗔𝗜𝗟𝗦
+├───────────────
+│ 👤𝐍𝐚𝐦𝐞 : 𝐌𝐃 𝐇𝐀𝐌𝐈𝐌
+│ 🚹 𝐆𝐞𝐧𝐝𝐞𝐫 : 𝐌𝐚𝐥𝐞
+│ ❤️ 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧 : 𝐈𝐧 𝐚 𝐑𝐞𝐥𝐞𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐟 𝐅𝐨𝐫 💝 [𝐀𝐟𝐢𝐟𝐚]💝
+│ 🎂 𝐀𝐠𝐞 : 𝟏𝟖+
+│ 🕌 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧 : 𝐈𝐬𝐥𝐚𝐦
+│ 🎓 𝐄𝐝𝐮𝐜𝐚𝐭𝐢𝐨𝐧 : 𝐇𝐒𝐂 (𝟐𝟎𝟐𝟔)
+│ 🏡 𝐀𝐝𝐝𝐫𝐞𝐬𝐬 : 𝐍𝐨𝐚𝐤𝐡𝐚𝐥𝐢 . 𝐁𝐚𝐧𝐠𝐥𝐚𝐝𝐞𝐬𝐡
+└───────────────⭓
+
+┌───────────────⭓
+│ 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
+├───────────────
+│ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸:
+│ https://fb.com/61580008498093
+│ 💬 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽:
+│ https://wa.me/𝐬𝐢𝐦 𝐧𝐚𝐢 🙎‍♂️
+└───────────────⭓`;
+
+    const cacheDir = path.join(__dirname, "cache");
+    const imgPath = path.join(cacheDir, "owner.jpg");
+
+    if (!fs.existsSync(cacheDir)) fs.mkdirSync(cacheDir);
+
+    const imgLink = "https://i.imgur.com/jpycfTa.jpeg";
+
+    const send = () => {
+      api.sendMessage(
+        {
+          body: ownerText,
+          attachment: fs.createReadStream(imgPath)
+        },
+        event.threadID,
+        () => fs.unlinkSync(imgPath),
+        event.messageID
+      );
+    };
+
+    request(encodeURI(imgLink))
+      .pipe(fs.createWriteStream(imgPath))
+      .on("close", send);
+  }
+};
